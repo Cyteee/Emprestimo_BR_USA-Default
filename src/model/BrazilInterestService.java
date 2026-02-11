da6a1bc7d8a2d6323c42bef6d0e7c0e3ab7b32df
Cyteee <coyotemalandro@gmail.com>
2026-02-11 19:45:11 -0300

    .

--- src/model/BrazilInterestService.java
+++ src/model/BrazilInterestService.java
@@ -2,27 +2,8 @@ package model;
 
 public class BrazilInterestService implements InterestService{
 
-    private double amount;
-
-    public BrazilInterestService() {
-        this.amount = amount;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
     @Override
     public double getInterestRate() {
         return 2.0;
-    }
-
-    @Override
-    public double getPaymentAmount(double amount, double months) {
-        return InterestService.super.getPaymentAmount(amount, months);
     }
 }
--- src/model/InterestService.java
+++ src/model/InterestService.java
@@ -6,8 +6,8 @@ public interface InterestService{
 
     double getInterestRate();
 
-    default double getPaymentAmount(double amount, double months){
-        if (amount < 0){
+    default double getPaymentAmount(double amount, int months){
+        if (amount < 1){
             throw new InvalidParameterException("");
         }
         return amount * Math.pow(1.0 + getInterestRate() / 100.0, months);
